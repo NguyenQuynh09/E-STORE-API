9ff6063e4108cae5150aa2dbbbd5f14dcb739c0d
NguyenQuynh09 <dunguyenquynh127@gmail.com>
2025-03-08 23:17:05 +0700

    FIX API

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const express=require("express")
 const cors=require("cors")
 
 const app=express()
-
-
 app.use(express.json())
 app.use(cors())
 
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -4,7 +4,6 @@ const Product = require("../models/product.model");
 
 async function createProduct(reqData){
     let topLevel = await Category.findOne({name:reqData.topLevelCategory});
-
     if(!topLevel){
         topLevel = new Category({
             name:reqData.topLevelCategory,
